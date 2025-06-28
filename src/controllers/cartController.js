2d618f26f8641a4690fc394131e57080038cfb5a
Chandar-uoo <chandru666v@gmail.com>
2025-06-28 12:18:26 +0530

    refactor : logic of Cart from controller moven to sevice folder

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -2,99 +2,49 @@ const express = require("express");
 const productModel = require("../models/productModel");
 const { default: mongoose } = require("mongoose");
 const validator = require("validator");
-const AppError = require("../utils/AppError")
+const AppError = require("../utils/AppError");
+
+const cartServices = require("../services/cartServices");
+
 exports.addToCart = async (req, res) => {
 
-        const user = req.user;
-        if (!req.user) {
-            throw new AppError("Unauthorized", 401);
-        }
-        const { id } = req.params;
-        if (!mongoose.Types.ObjectId.isValid(id)) {
-            throw new AppError("Bad Request", 400);
-        }
-        const product = await productModel.findById(id);
-        if (!product) {
-            throw new AppError("Bad Request", 400);
-        }
-        const findProduct = user.cart.find(item => item.productId.toString() === id.toString());
-        if (findProduct) {
-            findProduct.quantity += 1;
-            await user.save();
-            return res.status(200).json({
-                success:true,
-                message: "sucess of add",
-                result: {
-                    _id: findProduct._id
-                }
-            })
-
-        }
-        const newEntry = user.cart.push({
-            productId: product._id,
-            quantity: 1
-        });
-
-        await user.save();
+        const {newItem,user} = await cartServices.addToCartService(req,res);
 
         res.status(200).json({
             success:true,
-            message: "successfull add to cart",
-            result: user.cart[newEntry - 1]
+            message: "successfully item added to cart",
+            result: user.cart[newItem - 1]
         });
    
 }
 exports.readCart = async (req, res) => {
 
-        const user = req.user;
-        if (!req.user) {
-            throw new AppError("Unauthorized", 401);
-        }
-        const data = user.cart;
+        const data = await cartServices.readCartService(req,res);
+
         res.status(200).json({
             success:true,
-            message: "sucees",
+            message: "success of fetching data",
             result: data
         })
     
 }
 exports.updateCart = async(req,res)=>{
 
-        const {id} = req.params;
-        const {quantity} = req.body;
-        if (!Number.isInteger(quantity)) {
-            throw new AppError("Quantity must be an integer", 400);
-        }
-        const user = req.user;
-        const findProduct = user.cart.find(item => item.productId.toString() === id.toString());
-        if (!findProduct) {
-            throw new AppError("Invalid details", 400);
-        }
-       const result = findProduct.quantity +=quantity;
-        await user.save();
+       const findProduct = await cartServices.updateCartService(req,res);
+
         res.status(200).json({
             success:true,
             message:"sucessful",
             data : findProduct,
         })
 
- }
- exports.deleteCart = async (req,res) => {
-    
-         const {id} = req.params;
-         const user = req.user;
-         if (!mongoose.Types.ObjectId.isValid(id)) {
-            throw new AppError("Bad Request: Invalid ID", 400);
-        }
-          const product = await productModel.findById(id);
-          if (!product) {
-            throw new AppError("Product not found", 400);
-        }
-          user.cart = user.cart.filter(item => item.productId.toString() !== id.toString());
-          await user.save();
+}
+exports.deleteCart = async (req,res) => {
+    const product = await cartServices.deleteCartServices(req,res);
+
           res.status(200).json({
             sucess:true,
              message:"sucess",
-             result:id
+             result:product._id
           })
-  }
+}
---
+++ src/services/cartServices.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const { default: mongoose } = require("mongoose");
+const validator = require("validator");
+const productModel = require("../models/productModel");
+const AppError = require('../utils/AppError');
+
+exports.addToCartService = async (req, res) => {
+
+    const user = req.user;
+    if (!req.user) {
+        throw new AppError("Unauthorized", 401);
+    }
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        throw new AppError("Bad Request", 400);
+    }
+    const product = await productModel.findById(id);
+    if (!product) {
+        throw new AppError("Bad Request", 400);
+    }
+    const findProduct = user.cart.find(item => item.productId.toString() === id.toString());
+    if (findProduct) {
+        findProduct.quantity += 1;
+        await user.save();
+        return res.status(200).json({
+            success:true,
+            message: "sucessfully item quantity updated",
+            result: {
+                _id: findProduct._id
+            }
+        })
+
+    }
+    const newItem = user.cart.push({
+        productId: product._id,
+        quantity: 1
+    });
+
+    await user.save();
+    return {newItem,user};
+}
+
+exports.readCartService = async (req, res) => {
+
+        const user = req.user;
+        if (!req.user) {
+            throw new AppError("Unauthorized", 401);
+        }
+        const data = user.cart;
+        return data;
+}
+
+exports.updateCartService = async(req,res)=>{
+
+    const {id} = req.params;
+    const {quantity} = req.body;
+    if (!Number.isInteger(quantity)) {
+        throw new AppError("Quantity must be an integer", 400);
+    }
+    const user = req.user;
+    const findProduct = user.cart.find(item => item.productId.toString() === id.toString());
+    if (!findProduct) {
+        throw new AppError("Invalid details", 400);
+    }
+   const result = findProduct.quantity +=quantity;
+    await user.save();
+    return findProduct;
+}
+
+exports.deleteCartServices = async (req,res) => {
+    
+    const {id} = req.params;
+    const user = req.user;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+       throw new AppError("Bad Request: Invalid ID", 400);
+   }
+     const product = await productModel.findById(id);
+     if (!product) {
+       throw new AppError("Product not found", 400);
+   }
+     user.cart = user.cart.filter(item => item.productId.toString() !== id.toString());
+
+     await user.save();
+
+     return product;
+}
