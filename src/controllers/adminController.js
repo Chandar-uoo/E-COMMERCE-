125308aa16c7d19110e81db8eae8ca2057cd358d
Chandar-uoo <chandru666@vgmail.com>
2025-07-26 22:23:57 +0530

    chore removed unused code from this file

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -73,28 +73,11 @@ exports.fetchOrder = async (req, res) => {
     })
 }
 
-// fetch all orders =  paid & processing
-
-exports.ordersToFullfill = async (req, res) => {
-    const { orders, total, limit, page } = await adminServices.ordersToFullfillService(req, res);
-    res.status(200).json({
-        success: true,
-        message: "admin fetched all order with payment status paid and order status proceesing",
-        result: orders,
-        totalItems: total,
-        totalPages: Math.ceil(total / limit),
-        currentPage: page,
-        limit: limit,
-        hasNextPage: page < Math.ceil(total / limit),
-        hasPrevpage: page > 1
-    })
-}
 exports.updateOrderStatus = async (req, res) => {
-    const { status } = req.body;
-    const data = await adminServices.updateOrderStatusService(req, res);
+    const data = await adminServices.updateOrderStatusService(req,res);
     res.status(200).json({
         success: true,
-        message: `admin replaced order status proceesing to ${status} `,
+        message: "admin replaced order status proceesing to shipped",
         result: data
     })
 }
