267537131bde12431963ba87b0ce095f4833da51
Chandar-uoo <chandru666@vgmail.com>
2025-07-26 22:30:41 +0530

    refactor update status api and removed unused code

--- src/services/adminServices.js
+++ src/services/adminServices.js
@@ -1,142 +1,153 @@
 const productModel = require("../models/productModel");
 const userModel = require("../models/user");
-const orderModel = require("../models/orderModel")
+const orderModel = require("../models/orderModel");
 const { default: mongoose } = require("mongoose");
 const AppError = require("../utils/AppError");
 exports.fetchProductService = async (req, res) => {
-    const page = Math.max(1, Number(req.query.page) || 1);
-    const limit = Math.min(100, Math.max(Number(req.query.limt) || 10));
-    const skip = (page - 1) * limit;
-    const products = await productModel.find().limit(limit).skip(skip);
-    const total = await productModel.countDocuments();
-    return { products, total, limit, page };
-}
+  const page = Math.max(1, Number(req.query.page) || 1);
+  const limit = Math.min(100, Math.max(Number(req.query.limt) || 10));
+  const skip = (page - 1) * limit;
+  const products = await productModel.find().limit(limit).skip(skip);
+  const total = await productModel.countDocuments();
+  return { products, total, limit, page };
+};
 
-// add product 
+// add product
 
 exports.addProductService = async (req, res) => {
-    const { ProductName, category, description, price, img, stock, rating, } = req.body.product;
+  const { ProductName, category, description, price, img, stock, rating } =
+    req.body.product;
 
-    if (!ProductName || !category || !description || !price || !img || !stock || !rating) {
-        return res.status(400).json({
-            sucsess: false,
-            message: "please enter all details",
-        })
-    }
-
-    const existingProduct = await productModel.findOne({ ProductName });
-    if (existingProduct) {
-        throw new AppError("product is already available", 401)
-    }
-
-    const newProduct = await productModel.create({
-        ProductName,
-        category,
-        description,
-        price,
-        img,
-        stock,
-        rating
+  if (
+    !ProductName ||
+    !category ||
+    !description ||
+    !price ||
+    !img ||
+    !stock ||
+    !rating
+  ) {
+    return res.status(400).json({
+      sucsess: false,
+      message: "please enter all details",
     });
+  }
 
-    return newProduct;
-}
+  const existingProduct = await productModel.findOne({ ProductName });
+  if (existingProduct) {
+    throw new AppError("product is already available", 401);
+  }
+
+  const newProduct = await productModel.create({
+    ProductName,
+    category,
+    description,
+    price,
+    img,
+    stock,
+    rating,
+  });
+
+  return newProduct;
+};
 // update product
 exports.updateProductService = async (req, res) => {
-  
-   const { id,updateFields  } = req.body;
+  const { id, updateFields } = req.body;
 
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-        throw new AppError("Bad Request", 400);
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    throw new AppError("Bad Request", 400);
+  }
+  const product = await productModel.findById(id);
+  if (!product) {
+    throw new AppError("product not found", 404);
+  }
+
+  const allowedFields = [
+    "ProductName",
+    "category",
+    "description",
+    "price",
+    "img",
+    "stock",
+    "rating",
+  ];
+  const updates = {};
+  for (let key in updateFields) {
+    if (allowedFields.includes(key)) {
+      updates[key] = updateFields[key];
     }
-    const product = await productModel.findById(id);
-    if (!product) {
-        throw new AppError("product not found", 404);
-    }
+  }
 
-    const allowedFields = ["ProductName", "category", "description", "price", "img", "stock", "rating"];
-    const updates = {};
-    for (let key in updateFields) {
-        if (allowedFields.includes(key)) {
-            updates[key] = updateFields[key];
-        }
-    }
+  const updateProduct = await productModel.findByIdAndUpdate(id, updates, {
+    new: true,
+    runValidators: true,
+  });
 
-    const updateProduct = await productModel.findByIdAndUpdate(id, updates, {
-        new: true,
-        runValidators: true
-    });
-
-    return updateProduct;
+  return updateProduct;
 };
 // delete product
 exports.deleteProductSevice = async (req, res) => {
-    const { id } = req.params;
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-        throw new AppError("Bad Request", 400);
-    }
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    throw new AppError("Bad Request", 400);
+  }
 
-    const product = await productModel.findById(id);
-    if (!product) {
-        throw new AppError("product not found", 404);
-    }
+  const product = await productModel.findById(id);
+  if (!product) {
+    throw new AppError("product not found", 404);
+  }
 
-    const deleteProduct = await productModel.findByIdAndDelete(id);
-    return deleteProduct;
-}
+  const deleteProduct = await productModel.findByIdAndDelete(id);
+  return deleteProduct;
+};
 // fetch user
 exports.fetchUserService = async (req, res) => {
-    const page = Math.max(1, Number(req.query.page) || 1);
-    const limit = Math.min(100, Math.max(Number(req.query.limt) || 10));
-    const skip = (page - 1) * limit;
-    const users = await userModel.find({}).limit(limit).skip(skip);
-    const total = await userModel.countDocuments();
-    return { users, total, limit, page };
-}
+  const page = Math.max(1, Number(req.query.page) || 1);
+  const limit = Math.min(100, Math.max(Number(req.query.limt) || 10));
+  const skip = (page - 1) * limit;
+  const users = await userModel.find({}).limit(limit).skip(skip);
+  const total = await userModel.countDocuments();
+  return { users, total, limit, page };
+};
 // fetchOrders
 exports.fetchOrdersService = async (req, res) => {
-    const page = Math.max(1, Number(req.query.page) || 1);
-    const limit = Math.min(100, Math.max(Number(req.query.limt) || 10));
-    const skip = (page - 1) * limit;
-    const orders = await orderModel.find({}).limit(limit).skip(skip);
-    const total = await orderModel.countDocuments();
-    return { orders, total, limit, page };
-}
-// order to fullfill
-exports.ordersToFullfillService = async (req, res) => {
-    const page = Math.max(1, Number(req.query.page) || 1);
-    const limit = Math.min(100, Math.max(Number(req.query.limt) || 10));
-    const skip = (page - 1) * limit;
-    const filter = {
-        $and: [
-            { paymentStatus: "paid" },
-            { orderStatus: "processing" }
-        ]
-    }
-    const orders = await orderModel.find(filter).limit(40).skip(skip);
-    const total =  await orderModel.countDocuments(filter);
-    return { orders, total, limit, page };
-}
+  const page = Math.max(1, Number(req.query.page) || 1);
+  const limit = Math.min(100, Math.max(Number(req.query.limt) || 10));
+  const skip = (page - 1) * limit;
+  const { orderStatus } = req.query;
+  if (!orderStatus) {
+    throw new AppError("orderStatus is required", 400);
+  }
+  const filter = {};
+  if (orderStatus !== "all") {
+    filter.orderStatus = orderStatus;
+  }
+  const orders = await orderModel
+    .find(filter)
+    .populate({
+      path: "items.productId",
+      select: "ProductName price img category description",
+    })
+    .limit(limit)
+    .skip(skip);
+
+  const total = await orderModel.countDocuments();
+
+  return { orders, total, limit, page };
+};
 // update order as shipped
 exports.updateOrderStatusService = async (req, res) => {
-    const { id } = req.params;
-    const changeOrderStatus = req.body.status;
-    // checking is the payment is paid for the product
-    const order = await orderModel.findById(id);
-    // checking the changeOrderStatus is valid 
-    const allowedStatus = ["delivered", "failure", "shipped"];
-    if (!allowedStatus.includes(changeOrderStatus)) {
-        throw new AppError("Invalid Status to change order", 404);
-    }
-    const status = order.paymentStatus === "paid" ? true : false;
-    if (!status) {
-        return res.status(409).json({
-            success: false,
-            message: "payment has not been done for the product"
-        })
-    }
-    order.orderStatus = changeOrderStatus;
-    await order.save();
-    return order;
-
-}
+  const { id } = req.params;
+  // checking is the payment is paid for the product
+  const order = await orderModel.findById(id);
+  const status = order.paymentStatus === "paid" ? true : false;
+  if (!status) {
+    return res.status(409).json({
+      success: false,
+      message: "payment has not been done for the product",
+    });
+  }
+  order.orderStatus = "shipped";
+  await order.save();
+  return order;
+};
