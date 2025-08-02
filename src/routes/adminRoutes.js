05f470e4e010aa98336dfe2e60bd4967faf5d858
Chandar-uoo <chandru666@vgmail.com>
2025-08-02 13:13:16 +0530

    chore added middle ware on route

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -4,16 +4,16 @@ adminRouter.use(express.json());
 const catchAsync = require("../utils/catchAsync");
 const {roleCheck} =  require("../middleware/roleValidator");
 const adminController = require("../controllers/adminController");
-
+const { validateProductInput} =  require("../middleware/productValidator")
 const {auth}=  require("../middleware/authValidator")
 
 // get all profucts
 adminRouter.get("/products",auth,roleCheck("admin"),catchAsync(adminController.fetchProduct));
 
 // add products
-adminRouter.post("/add-product",auth,roleCheck("admin"),catchAsync(adminController.addProduct));
+adminRouter.post("/add-product",auth,roleCheck("admin"),validateProductInput,catchAsync(adminController.addProduct));
 // edit products
-adminRouter.patch("/update-product",auth,roleCheck("admin"),catchAsync(adminController.updateProduct))
+adminRouter.patch("/update-product",auth,roleCheck("admin"),validateProductInput,catchAsync(adminController.updateProduct))
 // delete product 
 adminRouter.delete("/delete-product/:id",auth,roleCheck("admin"),catchAsync(adminController.deleteProduct));
 // fetch order
@@ -21,6 +21,7 @@ adminRouter.get("/orders",auth,roleCheck("admin"),catchAsync(adminController.fet
 // fetch user
 adminRouter.get("/user",auth,roleCheck("admin"),catchAsync(adminController.fetchUser));
 //fetch order = paid & paymentStatus processing
+adminRouter.patch("/update-Order-Status/:id",auth,catchAsync(adminController.updateOrderStatus));
 adminRouter.get("/orders-to-fullfill",auth,roleCheck("admin"),catchAsync(adminController.filterOrdersStatus));
 
 module.exports = {adminRouter}; 
