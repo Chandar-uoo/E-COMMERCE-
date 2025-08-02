7efcd5d6ceb9bffb0a67013719303e3e41f7477b
Chandar-uoo <chandru666@vgmail.com>
2025-08-02 13:13:49 +0530

    chore created route to handle update on user

--- src/routes/userRoutes/userRouter.js
+++ src/routes/userRoutes/userRouter.js
@@ -5,6 +5,8 @@ userRouter.use(express.json());
 const userController = require("../../controllers/userController");
 const catchAsync = require("../../utils/catchAsync")
 userRouter.get('/userCheck',auth,catchAsync(userController.userCheck))
+userRouter.post("/updateUser",auth,catchAsync(userController.updateUserDetails))
+userRouter.patch("/updatePassword",auth,catchAsync(userController.updateUserPassword))
  module.exports = {
     userRouter,
  } 
