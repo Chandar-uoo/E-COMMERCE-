b2a73a68a11f27480e43b3d9ae9b4457eb613b1d
Chandar-uoo <chandru666@vgmail.com>
2025-08-28 16:28:49 +0530

    feat added pagination & updated response structure

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,30 +1,37 @@
-const  productServices = require("../services/productServices");
+const productServices = require("../services/productServices");
 
 exports.searchProduct = async (req, res) => {
+  const{ products,total,limit,page} = await productServices.getProductbyFilterService(req, res);
 
-    const  products  =  await productServices.getProductbyFilterService(req,res);
-
-    res.status(200).json({
-        success: true,
-        message: 'successfull',
-        result: products,
-    })
-}
+  res.status(200).json({
+    success: true,
+    message: "Products fetched successfully",
+    result: {
+      data: products,
+      pagination: {
+        totalItems: total,
+        totalPages: Math.ceil(total / limit),
+        currentPage: page,
+        limit: limit,
+        hasNextPage: page < Math.ceil(total / limit),
+        hasPrevPage: page > 1,
+      },
+    },
+  });
+};
 exports.allProducts = async (req, res) => {
-
-    const  products = await productServices.allProductService(req,res) ;
-    res.status(200).json({
-        sucess: true,
-        message: "suceess",
-        result: products,
-    });
-}
+  const products = await productServices.allProductService(req, res);
+  res.status(200).json({
+    sucess: true,
+    message: "suceess",
+    result: products,
+  });
+};
 exports.singleProduct = async (req, res) => {
-
-    const data =  await productServices.singleProductService(req,res);
-    res.status(200).json({
-        success: true,
-        message: "sucess",
-        result: data
-    })
-}
+  const data = await productServices.singleProductService(req, res);
+  res.status(200).json({
+    success: true,
+    message: "sucess",
+    result: data,
+  });
+};
--- src/services/productServices.js
+++ src/services/productServices.js
@@ -1,46 +1,46 @@
 const { default: mongoose } = require("mongoose");
-const productModel =  require("../models/productModel");
- const AppError = require("../utils/AppError");
+const productModel = require("../models/productModel");
+const AppError = require("../utils/AppError");
 
 exports.getProductbyFilterService = async (req, res) => {
-     const {
-     search,
+  const {
+    search,
     category,
     brand,
     tags,
     minPrice,
     maxPrice,
     stock,
-    sortBy,         // "price_asc", "price_desc", "sold", "latest"
-    page = 1,
-    limit = 15,
+    sortBy, // "price_asc", "price_desc", "sold", "latest"
   } = req.query;
 
+  const page = Math.max(1, Number(req.query.page) || 1);
+  const limit = Math.min(100, Math.max(Number(req.query.limt) || 20));
+  const skip = (page - 1) * limit;
+  // Build filter object
+  const filter = {};
 
-    // Build filter object
-    const filter = {};
-    
-    if (category) filter.category = category;
-    if (brand) filter.brand = brand;
-    if(tags && tags.length > 0)filter.tags = {$in :tags.split(",")};
-    if(stock) filter.stock = stock;
+  if (category) filter.category = category;
+  if (brand) filter.brand = brand;
+  if (tags && tags.length > 0) filter.tags = { $in: tags.split(",") };
+  if (stock) filter.stock = stock;
 
-    // Regex-based partial search on title or brand
-if (search) {
-  const searchRegex = new RegExp(search, 'i');
-  filter.$or = [
-    { title: { $regex: searchRegex } },
-    { brand: { $regex: searchRegex } },
-    {category:{$regex:searchRegex}}
-  ];
-}
-    // range
-      if (minPrice || maxPrice) {
+  // Regex-based partial search on title or brand
+  if (search) {
+    const searchRegex = new RegExp(search, "i");
+    filter.$or = [
+      { title: { $regex: searchRegex } },
+      { brand: { $regex: searchRegex } },
+      { category: { $regex: searchRegex } },
+    ];
+  }
+  // range
+  if (minPrice || maxPrice) {
     filter.price = {};
     if (minPrice) filter.price.$gte = Number(minPrice);
     if (maxPrice) filter.price.$lte = Number(maxPrice);
   }
-    // Sorting Logic
+  // Sorting Logic
   let sort = {};
   switch (sortBy) {
     case "price_asc":
@@ -58,39 +58,34 @@ if (search) {
     default:
       sort.createdAt = -1;
   }
-    const products = await productModel
-      .find(filter)
-      .sort(sort) // Sort by price
-      .limit(limit)
-      .skip((page - 1) * limit)
-      .lean();
+  const [products, total] = await Promise.all([
+    productModel.find(filter).sort(sort).skip(skip).limit(limit).lean(),
+    productModel.countDocuments(filter),
+  ]);
 
-
-    return products;
+  return { products, total, limit, page };
 };
 
- exports.allProductService = async (req, res) => {
-     const products = await productModel.find().limit(30).lean();
-     return products;
- }
+exports.allProductService = async (req, res) => {
+  const products = await productModel.find().limit(30).lean();
+  return products;
+};
 
- exports.singleProductService =  async (req, res) => {
-
-    const { id } = req.params;
-    if ( !id || !mongoose.Types.ObjectId.isValid(id)) {
-        throw new AppError("Invalid request: ID is not valid", 400);
-    }
-    const data = await productModel.findById(id).populate({
+exports.singleProductService = async (req, res) => {
+  const { id } = req.params;
+  if (!id || !mongoose.Types.ObjectId.isValid(id)) {
+    throw new AppError("Invalid request: ID is not valid", 400);
+  }
+  const data = await productModel.findById(id).populate({
     path: "reviews",
-    options:{sort:{createdAt:1}},
+    options: { sort: { createdAt: 1 } },
     populate: {
-      path: "user",          // if you want user info inside each review
-      select: "name"   // only include name and email
-    }
+      path: "user", // if you want user info inside each review
+      select: "name", // only include name and email
+    },
   });
-    if (!data) {
-        throw new AppError("Invalid request: data not found", 404);
-    }
-    return data;
-}
-
+  if (!data) {
+    throw new AppError("Invalid request: data not found", 404);
+  }
+  return data;
+};
