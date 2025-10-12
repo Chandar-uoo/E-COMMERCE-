e146f5d95ebf9929bf749a11c2b65bf8516f9e69
Chandar-uoo <chandru666@vgmail.com>
2025-10-12 16:07:35 +0530

    fix: standardize user route paths for consistency

--- src/routes/userRoutes/userRouter.js
+++ src/routes/userRoutes/userRouter.js
@@ -4,9 +4,9 @@ const {auth} = require('../../middleware/authValidator')
 userRouter.use(express.json());
 const userController = require("../../controllers/userController");
 const catchAsync = require("../../utils/catchAsync")
-userRouter.get('/userCheck',auth,catchAsync(userController.userCheck))
-userRouter.patch("/updateUser",auth,catchAsync(userController.updateUserDetails))
-userRouter.patch("/updatePassword",auth,catchAsync(userController.updateUserPassword))
+userRouter.get('/user-check',auth,catchAsync(userController.userCheck))
+userRouter.patch("/update-user",auth,catchAsync(userController.updateUserDetails))
+userRouter.patch("/update-password",auth,catchAsync(userController.updateUserPassword))
 userRouter.post("/send-verification-otp",catchAsync(userController.userEmailOtpSend))
 userRouter.post("/verify-otp",catchAsync(userController.userEmailVerify))
  module.exports = {
